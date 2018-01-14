dc5ddb5ca9b9ffde6a3fdaf6685a98fc144030bb
方雅婧 <380890432@qq.com>
2018-01-14 11:18:17 +0800

    add some message type for akkademydb

--- akkademy-db/src/main/java/com/akkademy/AkkademyDb.java
+++ akkademy-db/src/main/java/com/akkademy/AkkademyDb.java
@@ -4,13 +4,17 @@ import akka.actor.AbstractActor;
 import akka.actor.Status;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
+import akka.io.Tcp;
 import akka.japi.pf.ReceiveBuilder;
+import com.akkademy.messages.Connected;
 import com.akkademy.messages.GetRequest;
 import com.akkademy.messages.KeyNotFoundException;
 import com.akkademy.messages.SetRequest;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class AkkademyDb extends AbstractActor {
     protected final LoggingAdapter log = Logging.getLogger(context().system(), this);
@@ -19,20 +23,47 @@ public class AkkademyDb extends AbstractActor {
     private AkkademyDb() {
         //调用父类的receive方法
         receive(ReceiveBuilder
-            .match(SetRequest.class, message -> {
-                log.info("Received Set request:{}", message);
-                map.put(message.key, message.value); })
-            .match(GetRequest.class, message -> {
-                log.info("Received Get request: {}", message);
-                Object value = map.get(message.key);
-                Object response = (value != null)
-                        ? value
-                        : new Status.Failure(new KeyNotFoundException(message.key));
-                sender().tell(response, self());
-            })
-            .matchAny(o ->
-                    sender().tell(new Status.Failure(new ClassNotFoundException()), self()))
-            .build());
+                .match(Connected.class, message -> {
+                    log.info("Received COnnected requesr:{}", message);
+                    sender().tell(new Connected(), self());
+                })
+                .match(List.class, message -> {
+                    message.forEach(x -> {
+                        if(x instanceof SetRequest) {
+                            SetRequest setRequest = (SetRequest)x;
+                            handleSetRequest(setRequest);
+                        }
+                        if(x instanceof GetRequest) {
+                            GetRequest getRequest = (GetRequest)x;
+                            handleGetRequest(getRequest);
+                        }
+                    });
+                })
+                .match(SetRequest.class, message ->
+                        handleSetRequest(message)
+                )
+                .match(GetRequest.class, message ->
+                        handleGetRequest(message)
+                )
+                .matchAny(o ->
+                        sender().tell(new Status.Failure(new ClassNotFoundException()), self()))
+                .build()
+        );
+    }
+
+    private void handleSetRequest(SetRequest message) {
+        log.info("Received Set request:{}", message);
+        map.put(message.key, message.value);
+        message.sender.tell(new Status.Success(message.key), self());
+    }
+
+    private void handleGetRequest(GetRequest message) {
+        log.info("Received Get request: {}", message);
+        Object value = map.get(message.key);
+        Object response = (value != null)
+                ? value
+                : new Status.Failure(new KeyNotFoundException(message.key));
+        sender().tell(response, self());
     }
 
 }
--- akkademy-db/src/main/java/com/akkademy/messages/SetRequest.java
+++ akkademy-db/src/main/java/com/akkademy/messages/SetRequest.java
@@ -1,13 +1,32 @@
 package com.akkademy.messages;
 
+import akka.actor.ActorRef;
+
 import java.io.Serializable;
 
 public class SetRequest implements Serializable{
     public final String key;
     public final Object value;
+    public final ActorRef sender;
 
     public SetRequest(String key, Object value) {
         this.key = key;
         this.value = value;
+        this.sender = ActorRef.noSender();
+    }
+
+    public SetRequest(String key, Object value, ActorRef sender) {
+        this.key = key;
+        this.value = value;
+        this.sender = sender;
+    }
+
+    @Override
+    public String toString() {
+        return "SetRequest{" +
+                "key='" + key + '\'' +
+                ", value=" + value +
+                ", sender=" + sender +
+                '}';
     }
 }
